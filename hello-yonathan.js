87a7acd44b1977a57760ab89bf905c6a6c55e411
Yonathan Nieves <yonathannieves17@gmail.com>
2024-11-18 10:24:16 -0500

    nodemon instalado

--- hello-yonathan.js
+++ hello-yonathan.js
@@ -6,7 +6,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello, Yonathan!\n')
+    res.end('Hello, Yonathan!\n Welcome')
 })
 
 server.listen(port, hostname, () => {
