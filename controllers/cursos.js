e9939fddbdb5018f8c29fcadca4a75b50b16456d
Yonathan Nieves <yonathannieves17@gmail.com>
2024-11-24 00:10:31 -0500

    Agregando y probando la funcionalidad para crear cursos. Endpoint de tipo POST

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -2,8 +2,29 @@ const express = require('express');
 const Curso = require('../models/curso_model')
 const ruta = express.Router()
 
-ruta.get('/', (req, res)=>{
-    res.json('Respuesta a petición GET de CURSOS funcionando correctamente...')
+// Función asincrona para crear cursos
+async function crarCurso(body){
+    let curso = new Curso({
+        titulo      : body.titulo,
+        descripcion : body.descripcion,
+        alumnos     : body.alumnos,
+        calificacion: body.calificacion
+    })
+    return await curso.save()
+}
+
+ruta.post('/', (req, res)=>{
+    let resultado = crarCurso(req.body)
+
+    resultado.then( curso => {
+        res.json({
+            curso 
+        })
+    }).catch( err => {
+        res.status(400).json({
+            err
+        })
+    })
 })
 
 module.exports = ruta
